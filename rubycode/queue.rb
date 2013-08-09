df291f666e8a677cd1ecb394bdf0c33bcff2045d
anjalicg <anjalicg2001@gmail.com>
2013-08-09 00:22:52 -0700

    finished Queue DS

--- rubycode/queue.rb
+++ rubycode/queue.rb
@@ -4,47 +4,92 @@ end
 class Queue
   #The FIFO data structure
   attr_accessor :first, :last, :size
-  def initialize(first,last)
+
+  def initialize(first, last)
     raise TypeError, "first and last should be of type nil or Node" if ((first!=nil and first.class.name!="Node") or (last!=nil and last.class.name!="Node"))
     @first=first
     @last=last
     @size=0
   end
+
   def add(node)
     #to add node to a queue
-    
+    if @first==nil
+      puts "empty q"
+      @first=node
+      @first.next=nil
+      @last=@first
+    else
+      temp=@last
+      @last=node
+      temp.next=node
+    end
+    @size+=1
+    puts "first node"
+    puts @first.data.to_str
+    puts "last node"
+    puts @last.data.to_str
   end
-    def delete()
-      node=nil
-      if @last==nil and @first==nil
-        puts "empty queue"
-        return node
-        elsif @last==@first
-        puts "One element leftqueue"
-        node=@last
-        @last=nil
-        @first=nil
-        @size-=1
-      else
-        puts "more than 1 element left"
-        node=@last
-        
+
+  def delete()
+    node=nil
+    if @last==nil and @first==nil
+      puts "empty queue"
+      return node
+    elsif @last==@first
+      puts "One element leftqueue"
+      node=@last
+      @last=nil
+      @first=nil
+      @size-=1
+    else
+      puts "more than 1 element left"
+      ###
+      node=@first
+      @first=node.next
+      @size-=1
+    end
+    return node
+
+
+  end
+
+  def size
+    return @size
+  end
+
+  def traverse
+    #puts "inside q traverse"
+    node=@first
+    i=1
+    while(node!=nil)
+      puts "Node #{i} : "+node.data.to_str()
+      node=node.next()
+      i+=1
+    end
+  end
+
+  def search(element)
+    node=@first
+    foundpos=-1
+    i=0
+    while(node!=nil)
+      if(node.data.to_str.match(element)) then
+        foundpos=i
       end
-      
-        
+      node=node.next()
+      i+=1
     end
-    def size
-      return @size
+    return foundpos
+  end
+
+  def empty?()
+    if @size==0 then
+      return true
+    else
+      return false
     end
-    def traverse
-    end
-    def search
-    end
-    def empty?()
-      if @size==0 then return true
-      else return false
-      end
-        
-    end
-      
+
+  end
+
 end
--- rubycode/test/testQueue.rb
+++ rubycode/test/testQueue.rb
@@ -5,39 +5,60 @@ require "../queue.rb"
 
 class TestQueue < Test::Unit::TestCase
   def test_queue_new
-    p=Person.new("name",20)
-    n=Node.new(p,nil)
-    assert_nothing_raised(TypeError){
-      q= Queue.new(n,n)
-      }
-    assert_raise(TypeError){
-      q= Queue.new(p,p)
-      }
-    assert_nothing_raised(TypeError){
-      q= Queue.new(nil,nil)
-      }
+    p=Person.new("name", 20)
+    n=Node.new(p, nil)
+    assert_nothing_raised(TypeError) {
+      q= Queue.new(n, n)
+    }
+    assert_raise(TypeError) {
+      q= Queue.new(p, p)
+    }
+    assert_nothing_raised(TypeError) {
+      q= Queue.new(nil, nil)
+    }
   end
+
   def test_member_methods()
-    q=Queue.new(Node.new(Person.new("name",20),nil),nil)
-    assert_respond_to(q,"add")
-    assert_respond_to(q,"delete")
-    assert_respond_to(q,"search")
-    assert_respond_to(q,"size")
-    assert_respond_to(q,"traverse")   
+    q=Queue.new(Node.new(Person.new("name", 20), nil), nil)
+    assert_respond_to(q, "add")
+    assert_respond_to(q, "delete")
+    assert_respond_to(q, "search")
+    assert_respond_to(q, "size")
+    assert_respond_to(q, "traverse")
   end
+
   def test_queue_function()
-    names=["apple","baby","carrot","dog","elephant","fig","grass","horse","ink"]
+    names=["apple", "baby", "carrot", "dog", "elephant", "fig", "grass", "horse", "ink"]
     age=20
     i=0
-    q=Queue.new(nil,nil)
-    names.each{|name|
-      n=Node.new(Person.new(name,age+i),nil)
+    q=Queue.new(nil, nil)
+    names.each { |name|
+      n=Node.new(Person.new(name, age+i), nil)
       puts "Is empty?:"+q.empty?().to_s()
       q.add(n)
       i+=1
-      }
-    
+      puts "Queue Size now:"+q.size().to_s
+    }
+    puts "Travesing..."
+    q.traverse()
+    puts "Searching................."
+    #puts nameStack.search("apple").to_s
+    assert_not_equal(-1, q.search("apple"), "'apple' was found")
+    assert_not_equal(-1, q.search("elephant"), "'elephant' was found")
+    #puts nameStack.search("elephant").to_s
+    assert_equal(-1, q.search("job"), "'Job' was not found")
+    #puts nameStack.search("ink").to_s
+    assert_not_equal(-1, q.search("ink"), "'ink' was found")
+    puts "Test deletion"
+    size=q.size
+    while (q.size!=0)
+      assert_equal(size,q.size)
+      q.delete().data.to_str
+      size-=1
+    end
+
+
   end
-  
-  
+
+
 end
